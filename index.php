1f478098a72a25980ba6b6f8d2838deb54338e00
Kyle <ixkalz@gmail.com>
2021-02-20 13:34:48 +0000

    Hello World!

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Index</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link href="css/style.css" rel="stylesheet">-->
+</head>
+
+<body>
+    <?php echo "Hello World!"; ?>
+    <!--<script src="js/script.js"></script>-->
+</body>
+</html>
