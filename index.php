ce7886897dd563ebd467d751217ae77eb330e8ae
Kyle <ixkalz@gmail.com>
2021-02-20 13:51:56 +0000

    Added composer.json and updated the author and description

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>Index</title>
-    <meta name="author" content="">
-    <meta name="description" content="">
+    <meta name="author" content="Kyle McPherson">
+    <meta name="description" content="Enterprise Web System Coursework">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--<link href="css/style.css" rel="stylesheet">-->
 </head>
